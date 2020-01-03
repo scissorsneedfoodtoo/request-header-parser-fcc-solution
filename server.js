d05d1adeb2860666f033f495e1b8773d9bc41548
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2020-01-03 11:43:10 +0900

    Final cleanup

--- server.js
+++ server.js
@@ -1,39 +1,34 @@
-'use strict';
-
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 
 const app = express();
-const portNum = process.env.PORT || 3000;
-
-// allow Cross Origin requests, for testing
-app.use(cors());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-// get ip info even if passing through a proxy like here
+// Get IP info even if passing through a proxy like here
 app.enable('trust proxy'); 
 
 app.route('/')
-  .get(function (req, res) {
+  .get((req, res) => {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
 app.route('/api/whoami')
-  .get(function (req, res) {
+  .get((req, res) => {
     res.json({ ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent'] });
   });
 
 // 404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.status(404)
     .type('text')
     .send('Not Found');
 })
 
-app.listen(portNum, function () {
+const portNum = process.env.PORT || 3000;
+
+app.listen(portNum, () => {
   console.log(`Listening on port ${portNum}`);
 });
 
-module.exports = app; // for testing
+module.exports = app; // For testing
